52a1e3817bc03ac82cc992f4c39b59e80eb532a6
fadilhakim <fadil.nylon@gmail.com>
2018-07-04 17:03:35 +0800

    env file

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 require('dotenv').config()
 
 const express = require('express')
-const bodyParser = require('body-parser')
 const http = require('http');
 const routes = require('./routes/routes.js')
 const app = express()
 
+const helmet = require('helmet');
+const compression = require('compression');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const helmet = require('helmet');
-const compression = require('compression');
 
 app.set('views', './views') // specify the views directory
 app.set('view engine', 'ejs') // register the template engine
 
 app.use(express.static(__dirname + '/public'));
+app.use(helmet(), compression(), cookieParser());
 
 
 app.get('/', (req, res) => res.render('home'))
